94c006dfd9d7da1abc925cfe73cd2f4d303a2397
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-01-15 00:00:06 +0530

    Update and rename NumPy - Arithmetic Operations to NumPy - Arithmetic Operations.py

---
+++ Week 3 - Exploratory Data Analysis/Basics of NumPy Arrays/NumPy - Arithmetic Operations.py
@@ -0,0 +1,51 @@
+# Addition of Arrays
+import numpy as np
+
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+res = np.add(a, b)
+print(res)
+
+
+# Subtraction of Arrays
+import numpy as np
+
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+res = np.subtract(a, b)
+print(res)
+
+# Multiplication of Arrays
+import numpy as np
+
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+res = np.multiply(a, b)
+print(res)
+
+
+# Division of Arrays
+import numpy as np
+
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+res = np.divide(a, b)
+print(res)
+
+
+# Exponentiation (Power)
+import numpy as np
+ 
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+res = np.power(a, b)
+print(res)
+
+
+# Modulus Operation
+import numpy as np
+ 
+a = np.array([5, 72, 13, 100])
+b = np.array([2, 5, 10, 30])
+res = np.mod(a, b)
+print(res)

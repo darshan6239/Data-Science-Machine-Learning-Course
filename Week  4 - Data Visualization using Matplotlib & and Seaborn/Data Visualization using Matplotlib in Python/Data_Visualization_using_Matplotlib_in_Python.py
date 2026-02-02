c3c0d9adb88cdfb49af708820d0d02bbfb7e51cb
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-02-02 22:40:14 +0530

    Update Data_Visualization_using_Matplotlib_in_Python.py

--- Week  4 - Data Visualization using Matplotlib & and Seaborn/Data Visualization using Matplotlib in Python/Data_Visualization_using_Matplotlib_in_Python.py
+++ Week  4 - Data Visualization using Matplotlib & and Seaborn/Data Visualization using Matplotlib in Python/Data_Visualization_using_Matplotlib_in_Python.py
@@ -146,3 +146,249 @@ plt.show()
 
    np.random.seed(0): Ensures same random values every time (reproducibility).
    np.random.rand(10, 10): Generates a 10×10 array of random numbers between 0 and 1. """
+
+
+"""How to Customize Matplotlib Visualizations
+   Customization in Matplotlib allows you to improve look and clarity of plots by adjusting elements like colors, styles, labels, titles and gridlines. It helps make visualizations more informative and visually appealing for better data communication. Let’s explore different ways to customize visualizations:
+
+   1. Customizing Line Chart
+   Line charts can be customized using various properties:
+
+   Color: Change the color of the line
+   Linewidth: Adjust the width of the line
+   Marker: Change the style of plotted points
+   Markersize: Change the size of the markers
+   Linestyle: Define the style of the line like solid, dashed, etc.
+   Example: This code creates a customized line chart with a green dashed line, thicker width, large circular markers and labeled axes and title. """
+import matplotlib.pyplot as plt
+
+x = [10, 20, 30, 40]
+y = [20, 25, 35, 55]
+
+plt.plot(x, y, color='green', linewidth=3, marker='o', markersize=15, linestyle='--')
+
+plt.title("Customizing Line Chart")
+plt.ylabel('Y-Axis')
+plt.xlabel('X-Axis')
+plt.show()
+
+#   2. Customizing Bar Chart
+#   Bar charts can be made more informative and visually appealing by customizing:
+
+"""Color: Fill color of the bars
+   Edgecolor: Color of the bar edges
+   Linewidth: Thickness of the edges
+   Width: Width of each bar
+   Example: This code creates a customized bar chart with green bars, blue edges, thicker border lines and labeled axes and title. """
+import matplotlib.pyplot as plt
+
+x = ['Thur', 'Fri', 'Sat', 'Sun']
+y = [170, 120, 250, 190]
+
+plt.bar(x, y, color='green', edgecolor='black', linewidth=2)
+
+plt.title("Customizing Bar Chart")
+plt.xlabel("Day")
+plt.ylabel("Total Bill")
+plt.show()
+
+#   3. Customizing Histogram Plot
+#   To make histogram plots more effective various customizations can be applied:
+
+"""Bins: Number of groups (bins) to divide data into
+   Color: Bar fill color
+   Edgecolor: Bar edge color
+   Linestyle: Style of the edges like solid, dashed, etc.
+   Alpha: Transparency level (0 = transparent, 1 = opaque)
+   Example: This code creates a customized histogram with green bars, blue edges, dashed border lines, semi-transparent fill and labeled axes and title. """
+import matplotlib.pyplot as plt
+
+x = [7, 8, 9, 10, 10, 12, 12, 12, 13, 14, 14, 15, 16, 16, 17,
+     18, 18, 19, 20, 20, 21, 22, 23, 24, 25, 25, 26, 28, 30,
+     32, 35, 36, 38, 40, 42, 44, 48, 50]
+
+plt.hist(x, bins=10, color='green', edgecolor='blue',linestyle='--', alpha=0.5)
+
+plt.title("Customizing Histogram Plot")
+plt.xlabel("Total Bill")
+plt.ylabel("Frequency")
+plt.show()
+
+#   4. Customizing Scatter Plot
+#   Scatter plots can be enhanced with:
+
+"""S: Marker size (single value or array)
+   C: Color of markers or sequence of colors
+   Marker: Marker style like circle, diamond, etc.
+   Linewidths: Width of marker borders
+   Edgecolor: Color of marker borders
+   Alpha: Blending value, between 0 (transparent) and 1 (opaque)
+   Example: This code creates a customized scatter plot using diamond-shaped markers, where color represents size, marker size reflects the total bill and transparency is added for better visualization. It          includes labeled axes and a title."""
+
+import matplotlib.pyplot as plt
+
+x = ['Thur', 'Fri', 'Sat', 'Sun', 'Thur', 'Fri', 'Sat', 'Sun']
+y = [170, 120, 250, 190, 180, 130, 260, 200]
+size = [2, 3, 4, 2, 3, 2, 4, 3]  
+bill = [170, 120, 250, 190, 180, 130, 260, 200]  
+
+plt.scatter(x, y, c=size, s=bill, marker='D', alpha=0.5)
+
+plt.title("Customizing Scatter Plot")
+plt.xlabel("Day")
+plt.ylabel("Total Bill")
+plt.show()
+
+#   5. Customizing Pie Chart
+#   To make pie charts more effective and visually appealing use:
+
+"""Explode: Moving the wedges of the plot
+   Autopct: Label the wedge with their numerical value.
+   Color: Colors of the slices
+   Sadow: Used to create a shadow effect
+   Example: This code creates a customized pie chart with colored slices, exploded segments for emphasis, percentage labels with two decimal places and a shadow effect for better visual appeal."""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+cars = ['AUDI', 'BMW', 'FORD','TESLA', 'JAGUAR',]
+data = [23, 13, 35, 15, 12]
+explode = [0.1, 0.5, 0, 0, 0]
+colors = ( "orange", "cyan", "yellow","grey", "green",)
+
+plt.pie(data, labels=cars, explode=explode, autopct='%1.2f%%',colors=colors, shadow=True)
+plt.show()
+
+#   Matplotlib’s Core Components: Figures and Axes
+#   Before we proceed let’s understand two classes which are important for working with Matplotlib.
+
+#   1. Figure class
+#   The Figure class represents the full drawing area or canvas that can hold one or more plots. It is created using the figure() function and lets user control the overall size, layout and background of the plot window.
+
+#   Syntax: matplotlib.figure.Figure(figsize=None, facecolor=None)
+#   Parameter:
+
+""" figsize: Sets size of the figure (width, height) in inches.
+facecolor: Sets background color of the figure.
+Example: This code demonstrates how to use Figure class to create a simple line plot. It sets figure size and background color, adds custom axes, plots data and labels the axes and title. """
+import matplotlib.pyplot as plt
+
+# Create a Figure with basic size and background color
+fig = plt.figure(figsize=(6, 4), facecolor='lightblue')
+
+# Add Axes to the Figure
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+x = [1, 2, 3, 4]
+y = [10, 20, 15, 25]
+ax.plot(x, y)
+
+plt.title("Simple Line Plot")
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.show()
+
+#   2. Axes Class
+#   Axes class represents actual plotting area where data is drawn. It is the most basic and flexible for creating plots or subplots within a figure. A single figure can contain multiple axes but each Axes object belongs to only one figure. It can create an Axes object using axes() function.
+
+#   Syntax:axes([left, bottom, width, height])
+""" Example: This code creates a figure using Figure class and adds a custom axes area to it. It then plots two line graphs one for x vs y and another for y vs x. The graph includes axis labels, a title and a legend for better clarity and presentation."""
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+x = [10, 20, 30, 40]
+y = [20, 25, 35, 55]
+
+fig = plt.figure(figsize=(5, 4))
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8]) 
+
+ax.plot(x, y)
+ax.plot(y, x)
+
+ax.set_title("Linear Graph")
+ax.set_xlabel("X-Axis")
+ax.set_ylabel("Y-Axis")
+ax.legend(labels=('line 1', 'line 2'))
+plt.show()
+
+""" Advanced Techniques for Visualizing Subplots
+We have learned how to add basic parts to a graph to show more information. One method can be by calling the plot function again and again with a different set of values as shown in the above example. Now let’s see how to draw multiple graphs in one figure using some Matplotlib functions and how to create subplots.
+
+1. Using add_axes()
+add_axes() method allows us to manually add axes to a figure in Matplotlib. It takes a list of four values [left, bottom, width, height] to specify the position and size of the axes.
+
+Example: This code creates two side-by-side plots using add_axes() one for x vs y and another for y vs x all in a single figure. """
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+x = [10, 20, 30, 40]
+y = [20, 25, 35, 55]
+
+fig = plt.figure(figsize=(10, 4)) 
+ax1 = fig.add_axes([0.1, 0.1, 0.35, 0.8]) 
+ax2 = fig.add_axes([0.55, 0.1, 0.35, 0.8])
+
+ax1.plot(x, y)
+ax2.plot(y, x)
+plt.show()
+
+""" 2. Using subplot()
+    The subplot() method adds a plot to a specified grid position within the current figure. It takes three arguments: the number of rows, columns and plot index.
+
+    Example: This code uses subplot() to create two plots side by side one for x vs y and the other for y vs x within same figure. """
+import matplotlib.pyplot as plt
+
+x = [10, 20, 30, 40]
+y = [20, 25, 35, 55]
+plt.figure()
+
+plt.subplot(121)
+plt.plot(x, y)
+
+plt.subplot(122)
+plt.plot(y, x)
+plt.show()
+
+""" 3. Using subplot2grid()
+   The subplot2grid() creates axes object at a specified location inside a grid and also helps in spanning the axes object across multiple rows or columns.
+
+   Example: This code uses subplot2grid() to place two stacked plots in a single figure one on top of the other by defining their positions in a 7-row grid layout."""
+import matplotlib.pyplot as plt
+
+x = [10, 20, 30, 40]
+y = [20, 25, 35, 55]
+
+axes1 = plt.subplot2grid (
+(7, 1), (0, 0), rowspan = 2, colspan = 1)
+
+axes2 = plt.subplot2grid (
+(7, 1), (2, 0), rowspan = 2, colspan = 1)
+
+axes1.plot(x, y)
+axes2.plot(y, x)
+plt.show()
+
+""" Saving Plots Using savefig()
+    When plots are created using Matplotlib, users may want to save them as image files for use in reports, presentations or sharing. Matplotlib offers savefig() function to save the current plot to a file. By       changing file extension, users can store the plot in different formats such as .png, .jpg, .pdf or .svg.
+
+   Example:"""
+import matplotlib.pyplot as plt
+
+year = ['2010', '2002', '2004', '2006', '2008']
+production = [25, 15, 35, 30, 10]
+
+plt.bar(year, production)
+
+plt.savefig("output.jpg")
+plt.savefig("output1", facecolor='y', bbox_inches="tight",pad_inches=0.3, transparent=True)
+
+
+
+
+
+
+
+
+
+
+
+

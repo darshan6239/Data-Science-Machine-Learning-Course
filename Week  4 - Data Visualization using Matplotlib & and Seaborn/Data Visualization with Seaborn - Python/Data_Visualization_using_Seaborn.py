ccdcac64770b08bc8002aae351a317cbbb542d44
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-02-05 23:52:52 +0530

    Update Data_Visualization_using_Seaborn.py

--- Week  4 - Data Visualization using Matplotlib & and Seaborn/Data Visualization with Seaborn - Python/Data_Visualization_using_Seaborn.py
+++ Week  4 - Data Visualization using Matplotlib & and Seaborn/Data Visualization with Seaborn - Python/Data_Visualization_using_Seaborn.py
@@ -129,3 +129,130 @@ df['Species'] = df['Species'].map({ 0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'
 sns.kdeplot(data=df[df['Species'] == 'Virginica'], x='sepal length (cm)', fill=True, label='Virginica')
 plt.legend()
 plt.show()
+
+
+#   How to Customize Seaborn Plots with Python?
+""" Customizing Seaborn plots increases their readability and visual appeal which makes the data insights clearer and more informative. Here are several ways we can customize our plots in Seaborn:
+
+1. Adding Titles and Axis Labels
+Adding descriptive titles and axis labels makes our plots more understandable and informative. Using Matplotlib's plt.title(), plt.xlabel() and plt.ylabel() to set titles and axis labels. """
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+iris = sns.load_dataset('iris')
+sns.scatterplot(x='sepal_length', y='sepal_width', data=iris)
+
+# Add plot title and axis labels
+plt.title('Sepal Length vs Sepal Width')
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Sepal Width (cm)')
+plt.show()
+
+""" 2. Built-in Styles and Grids in Seaborn
+Seaborn provides built-in styles that control the background and grid of your plots. These styles improve readability and can be chosen based on your presentation needs.
+
+Available Styles:
+
+darkgrid – Dark background with light gridlines. Great for clear contrast.
+whitegrid – White background with light gridlines. Ideal for statistical plots.
+dark – Dark background without gridlines. Clean and modern look.
+white – Plain white background without gridlines. Good for simple visuals.
+ticks – White background with axis ticks styled sharply. Suitable for publications. """
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_style("whitegrid")
+
+sns.boxplot(x='species', y='petal_length', data=sns.load_dataset('iris'))
+plt.title('Petal Length Distribution by Species')
+plt.show()
+
+""" 3. Customizing Color Palettes
+Seaborn makes it easy to enhance the appearance of plots using color palettes. You can choose from built-in palettes like "deep", "muted", or "bright" or define your own using sns.color_palette(). Customizing colors improves clarity and helps match your data’s theme or purpose.
+
+a) Using a Built-in Palette: """
+sns.set_palette("pastel") 
+
+sns.violinplot(x='species', y='petal_length', data=sns.load_dataset('iris'))
+plt.title('Petal Length Distribution by Species')
+plt.show()
+
+""" b) Using a Custom Palette: """
+custom_colors = ['#FF5733', '#33FFBD', '#335BFF']
+sns.set_palette(custom_colors)
+
+sns.violinplot(x='species', y='petal_length', data=sns.load_dataset('iris'))
+plt.title('Custom Colored Petal Length Distribution')
+plt.show()
+
+""" 4. Adjusting Figure Size and Aspect Ratio
+We can adjust the figure size using plt.figure(figsize=(width,height)) to control the plot's dimensions. This allows for better customization to fit different presentation or reports. """
+plt.figure(figsize=(10, 6))
+
+sns.lineplot(x='year', y='passengers', data=sns.load_dataset('flights'))
+plt.title('Number of Passengers Over Time')
+plt.show()
+
+
+""" 5. Adding Markers to Line Plots
+Markers can be added to Seaborn line plots using the marker argument to highlight data points. For example adding circular markers to the line plot using sns.lineplot(x='x', y='y' ,marker='o') """
+sns.lineplot(x='year', y='passengers', data=sns.load_dataset('flights'), marker='o')
+plt.title('Number of Passengers Over Time')
+plt.show()
+
+""" Visualizing Relationships and Patterns with Seaborn
+We’ll see various plots in Seaborn for visualizing relationships, distributions and trends across our dataset. These visualizations help to find hidden patterns and correlations in datasets with multiple variables.
+
+1. Pair Plots
+Pair plots are used explore relationships between several variables by generating scatter plots for every pair of variables in a dataset along with univariate distributions on the diagonal. This is useful for exploring datasets with multiple variables and seeing potential correlations.
+
+Syntax: sns.pairplot(data, hue=None) """
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_style("whitegrid")
+custom_palette = sns.color_palette("husl", 8)
+sns.set_palette(custom_palette)
+
+data = sns.load_dataset("iris")
+sns.pairplot(data, hue="species")
+plt.show()
+
+""" 2. Joint Plots
+Joint plots combine a scatter plot with the distributions of the individual variables. This allows for a quick visual representation of how the variables are distributed individually and how they relate to one another.
+
+Syntax: sns.jointplot(x, y, data, kind='scatter')"""
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = sns.load_dataset("tips")
+sns.jointplot(x="total_bill", y="tip", data=data, kind="scatter", color="#008B8B")
+plt.show()
+
+""" 3. Grid Plot
+Grid plots in Seaborn are used to create multiple subplots in a grid layout. Using Seaborn's FacetGrid we can visualize how variables interact across different categories which makesit easier to compare groups or conditions within our dataset.
+
+Syntax: g = sns.FacetGrid(data, col='column_name', row='row_name')
+g.map(sns.scatterplot, 'x', 'y') """
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips = sns.load_dataset("tips")
+
+plot=sns.FacetGrid(tips, col="time", row="sex")
+plot.map(sns.scatterplot, "total_bill", "tip")
+plt.show()
+
+""" Regression Plots: Visualizing Linear Relationships
+Seaborn simplifies the process of performing and visualizing regressions specifically linear regressions which is important for identifying relationships between variables, detecting trends and making predictions. It supports two primary functions for regression visualization:
+
+regplot(): This function plots a scatter plot along with a linear regression model fit.
+lmplot(): This function also plots linear models but provides more flexibility in handling multiple facets and datasets.
+Example: Let’s use a simple dataset to visualize a linear regression between two variables: x (independent variable) and y (dependent variable). """
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips = sns.load_dataset('tips')
+
+sns.regplot(x='total_bill', y='tip', data=tips, scatter_kws={'s':10}, line_kws={'color':'red'})
+plt.show()

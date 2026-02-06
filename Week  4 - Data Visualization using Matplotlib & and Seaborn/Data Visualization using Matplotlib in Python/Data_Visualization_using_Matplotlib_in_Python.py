d349c593966386ab73d634ff5491c16df93c6e11
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-02-06 22:59:55 +0530

    Update Data_Visualization_using_Matplotlib_in_Python.py

--- Week  4 - Data Visualization using Matplotlib & and Seaborn/Data Visualization using Matplotlib in Python/Data_Visualization_using_Matplotlib_in_Python.py
+++ Week  4 - Data Visualization using Matplotlib & and Seaborn/Data Visualization using Matplotlib in Python/Data_Visualization_using_Matplotlib_in_Python.py
@@ -381,7 +381,6 @@ year = ['2010', '2002', '2004', '2006', '2008']
 production = [25, 15, 35, 30, 10]
 
 plt.bar(year, production)
-
 plt.savefig("output.jpg")
 plt.savefig("output1", facecolor='y', bbox_inches="tight",pad_inches=0.3, transparent=True)
 

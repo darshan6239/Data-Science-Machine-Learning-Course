7fb3d42ce226bf2c430a083d3996a99396c4423e
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-02 18:07:39 +0530

    Create Feature_Selection_Techniques_in_Machine_Learning.py

---
+++ Week 5 - Introduction to Machine Leaning/Feature Selection Methods/Feature_Selection_Techniques_in_Machine_Learning.py
@@ -0,0 +1,26 @@
+""" Feature selection is the process of choosing only the most useful input features for a machine learning model. It helps improve model performance, reduces noise and makes results easier to understand.
+
+Helps remove irrelevant and redundant features
+Improves accuracy and reduces overfitting
+Speeds up model training
+Makes models simpler and easier to interpret"""
+
+# Need of Feature Selection
+""" Feature selection methods are essential in data science and machine learning for several key reasons:
+
+Improved Accuracy: Models learn better when trained on only important features.
+Faster Training: Fewer features reduce computation time.
+Greater Interpretability: With fewer inputs, understanding model behavior becomes easier.
+Avoiding the Curse of Dimensionality: Reduces complexity when working with high-dimensional data. """
+
+
+""" Choosing the Right Feature Selection Method
+Choice of feature selection method depends on several factors:
+
+Dataset size: Filter methods are generally faster for large datasets while wrapper methods might be suitable for smaller datasets.
+Model type: Some models like tree-based models, have built-in feature selection capabilities.
+Interpretability: If understanding the rationale behind feature selection is crucial, filter methods might be a better choice.
+Computational resources: Wrapper methods can be time-consuming, so consider our available computing power.
+With these feature selection methods we can easily improve performance of our model and reduce its computational cost.
+
+"""

7bba51eb6f4e9eaedfb33ee9c51e48a3e9172c53
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-03 20:01:50 +0530

    Create Handling_Categorical_Data_in_Python.py

---
+++ Week 5 - Introduction to Machine Leaning/Feature Selection Methods/Handling_Categorical_Data_in_Python.py
@@ -0,0 +1,76 @@
+""" Categorical data refers to features that contain a fixed set of possible values or categories that data points can belong to. Handling categorical data correctly is important because improper handling can lead to inaccurate analysis and poor model performance. In this article, we will see how to handle categorical data and its related concepts.
+
+Why Do We Need to Handle Categorical Data?
+Handling categorical data is important because:
+
+Algorithms Require Numerical Inputs: Most machine learning algorithms cannot directly process categorical data and need it to be converted into numerical formats.
+Inconsistent Categories: Categorical data contains inconsistencies like typos, case sensitivity or alternate spellings. We must standardize these to avoid treating them as separate categories.
+Remapping Categories: Some categories might need to be grouped for simplicity and relevance. For example, remapping rare categories into an "Other" group.
+Improves Model Performance: Proper encoding techniques like one-hot encoding or label encoding help models to understand the relationships of categories leading to better predictions.
+Handles Real-World Complexity: It is used in many domains such as E-commerce, Finance, Healthcare, etc making it robust to handle important features. """
+
+
+""" Implementation for Handling Categorical Data
+Here we will be using a Demographics dataset which has some incorrect, invalid or meaningless data (bogus values) due to human error while filling survey form or any other reason. You can download dataset from here.
+
+Step 1: Importing necessary Libraries
+We will be using Numpy, Pandas, Matplotlib, Seaborn and Scikit-learn libraries for its implementation. """ 
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+
+""" Step 2: Loading the Dataset
+We load the dataset into a Pandas DataFrame for manipulation. """
+file_path = '/content/demographics.csv'
+main_data = pd.read_csv(file_path)
+print(main_data.head())
+
+""" Step 3: Identifying and Removing Bogus Blood Types
+First we create a DataFrame containing all valid blood types to check for bogus values in the dataset:""" 
+valid_blood_type_list = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
+blood_type_categories = pd.DataFrame({'blood_type': valid_blood_type_list})
+print(blood_type_categories)
+
+#  Lets find bogus blood types by comparing the dataset values to this valid list:
+unique_blood_types_main = set(main_data['blood_type'])
+valid_blood_types_set = set(blood_type_categories['blood_type'])  
+bogus_blood_types = unique_blood_types_main.difference(valid_blood_types_set)
+bogus_blood_types
+
+#  Once the bogus values are found the corresponding rows can be dropped from the dataset.
+bogus_records_index = main_data['blood_type'].isin(bogus_blood_types)
+
+without_bogus_records = main_data[~bogus_records_index].copy()
+without_bogus_records['blood_type'].unique()
+
+#  Step 4: Handling Inconsistent Marriage Status Categories
+#  Checking the unique values in the marriage_status column:
+main_data['marriage_status'].unique()
+
+#  Standardizing the categories by converting all text to lowercase.
+inconsistent_data = main_data.copy()
+inconsistent_data['marriage_status'] = inconsistent_data['marriage_status'].str.lower()
+inconsistent_data['marriage_status'].unique()
+
+#  Now we will standardize the categories by stripping extra spaces:
+inconsistent_data['marriage_status'] = inconsistent_data['marriage_status'].str.strip()
+inconsistent_data['marriage_status'].unique()
+
+#  Step 5: Grouping Income into Meaningful Bins
+#  Numerical data like age or income can be mapped to different groups. Let us check income range to define bin intervals:
+print(f"Max income - {main_data['income'].max()}, Min income - {main_data['income'].min()}")
+
+#  Now, let us create the range and labels for the income feature. Pandas cut method is used here.
+income_bins = [40000, 75000, 100000, 125000, 150000, np.inf]
+income_labels = ['40k-75k', '75k-100k', '100k-125k', '125k-150k', '150k+']
+
+remapping_data = main_data.copy()
+remapping_data['income_groups'] = pd.cut(
+    remapping_data['income'],
+    bins=income_bins,
+    labels=income_labels
+)
+remapping_data.head()

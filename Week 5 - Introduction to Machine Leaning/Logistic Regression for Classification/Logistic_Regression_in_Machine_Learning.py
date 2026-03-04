9ea91cca3e710eff55ba043ddc93ea6ef131dab8
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-04 19:10:55 +0530

    Create Logistic_Regression_in_Machine_Learning.py

---
+++ Week 5 - Introduction to Machine Leaning/Logistic Regression for Classification/Logistic_Regression_in_Machine_Learning.py
@@ -0,0 +1,47 @@
+""" Logistic Regression is a supervised machine learning algorithm used for classification problems. Unlike linear regression which predicts continuous values it predicts the probability that an input belongs to a specific class.
+
+It is used for binary classification where the output can be one of two possible categories such as Yes/No, True/False or 0/1.
+It uses sigmoid function to convert inputs into a probability value between 0 and 1. """
+
+
+""" Implementation for Logistic Regression
+Now, let's see the implementation of logistic regression in Python. Here we will be implementing two main types of Logistic Regression:
+
+1. Binomial Logistic regression: 
+In binomial logistic regression, the target variable can only have two possible values such as "0" or "1", "pass" or "fail". The sigmoid function is used for prediction.
+
+We will be using sckit-learn library for this and shows how to use the breast cancer dataset to implement a Logistic Regression model for classification."""
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+X, y = load_breast_cancer(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=23)
+
+clf = LogisticRegression(max_iter=10000, random_state=0)
+clf.fit(X_train, y_train)
+
+acc = accuracy_score(y_test, clf.predict(X_test)) * 100
+print(f"Logistic Regression model accuracy: {acc:.2f}%")
+
+""" 2. Multinomial Logistic Regression:
+Target variable can have 3 or more possible types which are not ordered i.e types have no quantitative significance like “disease A” vs “disease B” vs “disease C”."""
+from sklearn.model_selection import train_test_split
+from sklearn import datasets, linear_model, metrics
+
+digits = datasets.load_digits()
+
+X = digits.data
+y = digits.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+
+reg = linear_model.LogisticRegression(max_iter=10000, random_state=0)
+reg.fit(X_train, y_train)
+
+y_pred = reg.predict(X_test)
+
+print(f"Logistic Regression model accuracy: {metrics.accuracy_score(y_test, y_pred) * 100:.2f}%")

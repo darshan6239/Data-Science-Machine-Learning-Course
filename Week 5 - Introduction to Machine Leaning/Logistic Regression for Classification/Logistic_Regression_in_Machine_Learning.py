9aebffd219d46533aa9229ebec05e718d87c5a69
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-05 19:35:55 +0530

    Update Logistic_Regression_in_Machine_Learning.py

--- Week 5 - Introduction to Machine Leaning/Logistic Regression for Classification/Logistic_Regression_in_Machine_Learning.py
+++ Week 5 - Introduction to Machine Leaning/Logistic Regression for Classification/Logistic_Regression_in_Machine_Learning.py
@@ -15,7 +15,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 X, y = load_breast_cancer(return_X_y=True)
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=23)
 
 clf = LogisticRegression(max_iter=10000, random_state=0)

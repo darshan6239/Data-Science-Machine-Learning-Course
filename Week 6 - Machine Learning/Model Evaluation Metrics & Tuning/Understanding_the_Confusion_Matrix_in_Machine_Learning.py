e75c69e5e92e6e451c9e906a79fda45d9bd61688
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-07 10:55:18 +0530

    Implement confusion matrix and classification report
    Added implementation details for confusion matrix and classification report.

--- Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/Understanding_the_Confusion_Matrix_in_Machine_Learning.py
+++ Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/Understanding_the_Confusion_Matrix_in_Machine_Learning.py
@@ -43,4 +43,50 @@ plt.gca().figure.subplots_adjust(bottom=0.2)
 plt.gca().figure.text(0.5, 0.05, 'Prediction', ha='center', fontsize=13)
 plt.show()
 
+#    Step 5: Classifications Report based on Confusion Metrics
+print(classification_report(actual, predicted))
 
+
+""" Confusion Matrix For Multi-class Classification
+In multi-class classification the confusion matrix is expanded to account for multiple classes.
+
+    1) Rows represent the actual classes (ground truth).
+    2) Columns represent the predicted classes.
+    3) Each cell in the matrix shows how often a specific actual class was predicted as another class.
+For example in a 3-class problem the confusion matrix would be a 3x3 table where each row and column corresponds to one of the classes. It summarizes the model's performance across all classes in a compact format. """" 
+
+
+#    Implementation of Confusion Matrix for Multi-Class classification using Python
+#    Step 1: Import the necessary libraries
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+import matplotlib.pyplot as plt
+
+#    Step 2: Create the NumPy array for actual and predicted labels
+""" 1) y_true: List of true labels.
+    2) y_pred: List of predicted labels by the model.
+    3) classes: A list of class names: 'Cat', 'Dog' and 'Horse' """
+y_true = ['Cat'] * 10 + ['Dog'] * 12 + ['Horse'] * 10
+y_pred = ['Cat'] * 8 + ['Dog'] + ['Horse'] + ['Cat'] * 2 + ['Dog'] * 10 + ['Horse'] * 8 + ['Dog'] * 2
+classes = ['Cat', 'Dog', 'Horse']
+
+""" Step 3: Generate and Visualize the Confusion Matrix
+
+    1) ConfusionMatrixDisplay: Creates a display object for the confusion matrix.
+    2) confusion_matrix=cm: Passes the confusion matrix (cm) to display.
+    3)display_labels=classes: Sets the labels (['Cat' , 'Dog' , 'Horse']) or the confusion matrix. """ 
+cm = confusion_matrix(y_true, y_pred, labels=classes)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classes)
+disp.plot(cmap=plt.cm.Blues)
+plt.title('Confusion Matrix', fontsize=15, pad=20)
+plt.xlabel('Prediction', fontsize=11)
+plt.ylabel('Actual', fontsize=11)
+plt.gca().xaxis.set_label_position('top')
+plt.gca().xaxis.tick_top()
+plt.gca().figure.subplots_adjust(bottom=0.2)
+plt.gca().figure.text(0.5, 0.05, 'Prediction', ha='center', fontsize=13)
+
+plt.show()
+
+#    Step 4: Print the Classification Report
+print(classification_report(y_true, y_pred, target_names=classes))

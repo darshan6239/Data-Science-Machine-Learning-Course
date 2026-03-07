ae785b3425819f107ba3bc1d796e64b56293ce18
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-07 11:15:04 +0530

    Create Precision_and_Recall_in_Machine_Learning.py

---
+++ Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/Precision_and_Recall_in_Machine_Learning.py
@@ -0,0 +1,7 @@
+""" Precision and recall are two evaluation metric used to check the performance of Machine Learning Model. Precision is the ratio of a model’s classification of all positive classifications as positive. Recall tells us how many of the actual positive items the model was able to find. Precision and recall helps in classification problems. In this article we will explain Precision and Recall with formula and example.
+
+1. Precision
+Precision is the ratio between the True Positives and all the Positives. It shows how many of the “yes” predictions made by the model were actually correct. It helps us reduce wrong “yes” guesses which are called false positives (FP). 
+
+2. Recall
+Recall tells us how well a model finds all the correct “yes” cases in the data. It checks how many real positive cases the model was able to correctly identify."""

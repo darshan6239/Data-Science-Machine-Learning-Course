7dadb4068aa5537d54572de84fc455071bf9f3cb
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-12 20:02:12 +0530

    Update AUC_ROC_Curve_in_Machine_Learning.py

--- Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/AUC_ROC_Curve_in_Machine_Learning.py
+++ Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/AUC_ROC_Curve_in_Machine_Learning.py
@@ -8,7 +8,8 @@ When to Use AUC-ROC
 AUC-ROC is effective when:
     The dataset is balanced and the model needs to be evaluated across all thresholds.
     False positives and false negatives are of similar importance. """ 
-    
+
+
 #    Implementation using two different models
 #    1. Installing Libraries
 #    We will be importing numpy, pandas, matplotlib and scikit learn.

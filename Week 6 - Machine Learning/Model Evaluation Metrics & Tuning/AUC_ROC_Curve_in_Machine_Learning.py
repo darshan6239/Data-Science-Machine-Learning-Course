df673c447e6c067e67d0835a89f7fc4840a6a629
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-12 20:02:03 +0530

    Update AUC_ROC_Curve_in_Machine_Learning.py

--- Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/AUC_ROC_Curve_in_Machine_Learning.py
+++ Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/AUC_ROC_Curve_in_Machine_Learning.py
@@ -6,7 +6,6 @@
     Specificity: measures the proportion of actual negatives that the model correctly identifies. It is calculated as 1 - FPR.
 When to Use AUC-ROC
 AUC-ROC is effective when:
-
     The dataset is balanced and the model needs to be evaluated across all thresholds.
     False positives and false negatives are of similar importance. """ 
     

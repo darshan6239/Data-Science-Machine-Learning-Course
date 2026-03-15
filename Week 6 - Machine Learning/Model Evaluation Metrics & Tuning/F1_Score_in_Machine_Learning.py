c59da8c94245b013248b95b3fa8e0b09310eb073
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-15 21:58:50 +0530

    Update F1_Score_in_Machine_Learning.py

--- Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/F1_Score_in_Machine_Learning.py
+++ Week 6 - Machine Learning/Model Evaluation Metrics & Tuning/F1_Score_in_Machine_Learning.py
@@ -18,6 +18,7 @@ f1_micro = f1_score(y_true, y_pred, average='micro')
 f1_macro = f1_score(y_true, y_pred, average='macro')
 f1_weighted = f1_score(y_true, y_pred, average='weighted')
 
+
 print("F1 score per class:", f1_per_class)
 print("Micro-average F1 score:", f1_micro)
 print("Macro-average F1 score:", f1_macro)

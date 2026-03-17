2670ca3cf4f45addb0ce9bac01da1446fab3fcf9
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-17 11:57:10 +0530

    Create ML_Heart_Disease_Prediction_Using_Logistic_Regression.py

---
+++ Week 6 - Machine Learning/Project 1 of ML/ML_Heart_Disease_Prediction_Using_Logistic_Regression.py
@@ -0,0 +1,7 @@
+""" 1: Importing Necessary Libraries
+We will import Numpy, Pandas, Matplotlib, Seaborn, Statsmodels and sklearn library in python."""
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import seaborn as sns

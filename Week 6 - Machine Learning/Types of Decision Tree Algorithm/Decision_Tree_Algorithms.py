a1b2e98233cfc272a6339f62d598c073ba0aec0c
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-17 23:20:17 +0530

    Create Decision_Tree_Algorithms.py

---
+++ Week 6 - Machine Learning/Types of Decision Tree Algorithm/Decision_Tree_Algorithms.py
@@ -0,0 +1,36 @@
+""" Decision trees are widely used machine learning algorithms and can be applied to both classification and regression tasks. They work by splitting data based on feature values, forming a tree-like structure where each leaf node gives a prediction. Their simplicity and easy interpretation make them widely used in many applications."""
+
+
+#  Types of Decision Tree Algorithms
+#        There are six different decision tree algorithms
+
+"""
+    1. ID3 (Iterative Dichotomiser 3)
+    2. C4.5
+    3. CART (Classification and Regression Trees)
+    4. CHAID (Chi-Square Automatic Interaction Detection)
+    5. MARS (Multivariate Adaptive Regression Splines)
+    6. Conditional Inference Trees
+
+    Here’s a short summary of all decision tree algorithms we have learned so far:
+
+    ID3: Uses information gain to split data and works well for classification but it is prone to overfitting and struggles with continuous data.
+    C4.5: Advance version of ID3 with gain ratio for both discrete and continuous data but struggle with noisy data.
+    CART: Used for both classification and regression task. It minimizes Gini impurity for classification and MSE for regression with pruning technique to prevent overfitting.
+    CHAID: Uses chi-square tests for splitting and is effective for large categorical datasets but not for continuous data.
+    MARS: Extended version of CART using piecewise linear functions to model non-linear relationships but it is computationally expensive.
+    Conditional Inference Trees: Uses statistical hypothesis testing for unbiased splits and handles various data types but it is slower than others. """
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+

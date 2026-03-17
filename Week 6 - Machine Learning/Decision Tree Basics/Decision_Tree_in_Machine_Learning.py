4a4f0afab8eaed4ae9bca733727b8083b662bfe0
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-17 23:06:54 +0530

    Create Decision_Tree_in_Machine_Learning.py

---
+++ Week 6 - Machine Learning/Decision Tree Basics/Decision_Tree_in_Machine_Learning.py
@@ -0,0 +1,6 @@
+""" A decision tree is a supervised learning algorithm used for both classification and regression tasks. It has a hierarchical tree structure which consists of a root node, branches, internal nodes and leaf nodes. It works like a flowchart that helps in making step by step decision, where:
+
+Internal nodes represent attribute tests
+Branches represent attribute values
+Leaf nodes represent final decisions or predictions.
+Decision trees are widely used due to their interpretability, flexibility and low preprocessing needs."""

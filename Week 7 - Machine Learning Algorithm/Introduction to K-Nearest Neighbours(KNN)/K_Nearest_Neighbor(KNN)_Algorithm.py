46fafa8a11b098c06c7bbff71cc05d90a093698c
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-20 22:54:44 +0530

    Create K_Nearest_Neighbor(KNN)_Algorithm.py

---
+++ Week 7 - Machine Learning Algorithm/Introduction to K-Nearest Neighbours(KNN)/K_Nearest_Neighbor(KNN)_Algorithm.py
@@ -0,0 +1,12 @@
+""" K‑Nearest Neighbor (KNN) is a simple and widely used machine learning technique for classification and regression tasks. It works by identifying the K closest data points to a given input and making predictions based on the majority class or average value of those neighbors.
+
+K-Nearest Neighbors is also called as a lazy learner algorithm because it does not learn from the training set immediately instead it stores the entire dataset and performs computations only at the time of classification.   
+
+What is 'K' in K Nearest Neighbour?
+In the k-Nearest Neighbours algorithm k is just a number that tells the algorithm how many nearby points or neighbors to look at when it makes a decision.
+
+Example: Imagine you're deciding which fruit it is based on its shape and size. You compare it to fruits you already know.
+
+    If k = 3, the algorithm looks at the 3 closest fruits to the new one.
+    If 2 of those 3 fruits are apples and 1 is a banana, the algorithm says the new fruit is an apple because most of its neighbors are apples."""
+

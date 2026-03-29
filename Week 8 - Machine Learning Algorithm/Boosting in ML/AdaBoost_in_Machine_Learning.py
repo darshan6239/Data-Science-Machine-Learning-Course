22ded80518fcd820263d85e78955a118d4670a2b
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-03-29 18:29:29 +0530

    Create AdaBoost_in_Machine_Learning.py

---
+++ Week 8 - Machine Learning Algorithm/Boosting in ML/AdaBoost_in_Machine_Learning.py
@@ -0,0 +1,11 @@
+"""  AdaBoost is a boosting technique that combines several weak classifiers in sequence to build a strong one. Each new model focuses on correcting the mistakes of the previous one until all data is correctly classified or a set number of iterations is reached.
+
+Think of it like in a class, a teacher focuses more on weak learners to improve its academic performance, similarly boosting work.   
+
+Step 1: Initial Model (B1)
+
+Step 2: Adjusting Weights (B2)
+
+Step 3: Further Adjustment (B3)
+
+Step 4: Final Strong Model (B4 - Ensemble Model)  """

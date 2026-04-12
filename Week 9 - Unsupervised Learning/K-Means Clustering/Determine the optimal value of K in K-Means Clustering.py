ad6d9c81b8a63e8cb91af76a132f78e02dbd528f
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-04-12 23:48:10 +0530

    Create Determine the optimal value of K in K-Means Clustering.py

---
+++ Week 9 - Unsupervised Learning/K-Means Clustering/Determine the optimal value of K in K-Means Clustering.py
@@ -0,0 +1,10 @@
+"""  Clustering techniques use raw data to form clusters based on common factors among various data points. Choosing the right number of clusters (K) in K-Means clustering is very important. If we choose the wrong value of K, the model may not find good patterns in the data. But selecting the best K manually is not easy.
+
+So we use different techniques to find the optimal value of K. In this article we will learn these techniques.
+
+Techniques to find Optimal value of k
+1. Elbow method  
+2. Silhouette Method
+3. Gap Statistic
+4. Davies-Bouldin Index  """
+

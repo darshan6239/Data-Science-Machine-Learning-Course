699a4d2dde99cfc985880720045e7e84b26b973c
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-04-15 16:59:32 +0530

    Create Intro.py

---
+++ Week 9 - Unsupervised Learning/Introduction to Dimensionality Reduction/Intro.py
@@ -0,0 +1,3 @@
+"""  When working with machine learning models, datasets with too many features can cause issues like slow computation and overfitting. Dimensionality reduction helps to reduce the number of features while retaining key information. It converts high-dimensional data into a lower-dimensional space while preserving important details.
+
+What-is-Dimensionality-Reduction-.webp  """
